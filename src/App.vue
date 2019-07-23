b0f2224ba7e489c643bd3c98914a356777118ad0
zacdaryl <zhaodaryl@gmail.com>
2019-07-23 16:22:55 +0800

    flex wrap

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div v-for="item in list" :key="item.id" class="item">
+        <div >
+          {{ item }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +19,10 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  data: () => ({
+    list: [1,2,3,4,5,6,7,8,9,10]
+  })
 }
 </script>
 
@@ -24,5 +34,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.row {
+  width: 300px;
+  /* height: 50px; */
+  display: flex;
+  /* flex-direction: row; */
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  background-color: cadetblue
+}
+
+.item {
+  height: 50px;
+  width: 50px;
+  background-color: antiquewhite;
+  margin: 5px;
 }
 </style>
